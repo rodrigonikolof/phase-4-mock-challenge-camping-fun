d976fcde43ed1ab68c5db08392c1d1b6ccacc259
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-03-29 11:38:54 +1100

    commit tree 882f354f303097706e89dcee37464f72400b4bd6

---
+++ app/models/camper.rb
@@ -0,0 +1,6 @@
+class Camper < ApplicationRecord
+    has_many :signups 
+    has_many :activities, through: :signups
+    validates :name, presence: true
+    validates :age, numericality: {greater_than: 7, less_than: 19}
+end
